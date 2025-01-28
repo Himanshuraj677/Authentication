001550899ab815a2f68569148e59308045531bd3
Himanshu Raj <himanshuraj6771@gmail.com>
2025-01-28 22:55:23 +0530

    Initial commit with test route

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/api/test', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+}); 
